6ab6598162632993a036157b207027d57a5b6d89
matsu4ki <matsu4ki07@gmail.com>
2020-12-02 08:35:37 +0900

    やり直し系コマンドを更新

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+# railsアプリケーションの作成
 docker-compose run web rails new . --force --no-deps --database=postgresql --skip-bundle
+
+# railsファイルが作成されるので、
+docker-compose build
+
+
+# !!!やり直し系コマンド!!!
+# 同しようもない場合はcloneし直したほうが早いかも
+# docker-compose down --rmi all --volumes --remove-orphans
+# git clean -fdx
+# git reset --hard HEAD
